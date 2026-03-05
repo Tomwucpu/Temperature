72130e239acbcd3c978b40e89c39c08144adf9cb
Tomwucpu <1367052657@qq.com>
2026-03-05 15:15:31 +0800

    添加了文章、标签、字数统计

--- src/utils/animation-utils.ts
+++ src/utils/animation-utils.ts
@@ -1,16 +1,30 @@
-export const animateValue = (el: Element, end: number) => {
-    const duration = 1000;
-    let startTimestamp: number | null = null;
-    const step = (timestamp: number) => {
-        if (!startTimestamp) startTimestamp = timestamp;
-        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-        const currentVal = Math.floor(progress * end);
-        el.textContent = Number(currentVal).toLocaleString();
-        if (progress < 1) {
-            window.requestAnimationFrame(step);
-        } else {
-            el.textContent = Number(end).toLocaleString();
-        }
-    };
-    window.requestAnimationFrame(step);
+export const animateValue = (
+	el: Element,
+	end: number,
+	formatter?: (val: number) => string,
+) => {
+	const duration = 1000;
+	let startTimestamp: number | null = null;
+	const step = (timestamp: number) => {
+		if (!startTimestamp) startTimestamp = timestamp;
+		const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+		const currentVal = Math.floor(progress * end);
+
+		if (formatter) {
+			el.textContent = formatter(currentVal);
+		} else {
+			el.textContent = Number(currentVal).toLocaleString();
+		}
+
+		if (progress < 1) {
+			window.requestAnimationFrame(step);
+		} else {
+			if (formatter) {
+				el.textContent = formatter(end);
+			} else {
+				el.textContent = Number(end).toLocaleString();
+			}
+		}
+	};
+	window.requestAnimationFrame(step);
 };
