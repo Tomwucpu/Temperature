b070128ee7ea77a2284c76453b65ed1d6087090d
Tomwucpu <1367052657@qq.com>
2026-03-05 14:29:00 +0800

    添加顶部工具栏

--- src/config.ts
+++ src/config.ts
@@ -46,6 +46,16 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Archive,
 		LinkPreset.About,
 		{
+			name: "工具",
+			children: [
+				{
+					name: "活动日历",
+					url: "https://tool.temperaturetw.top",
+					external: true,
+				},
+			],
+		},
+		{
 			name: "GitHub",
 			url: "https://github.com/Tomwucpu/Temperature", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
